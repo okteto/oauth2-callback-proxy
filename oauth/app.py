45ec1bb57bac8d5f566388a0b3886fd036687809
Ramiro Berrelleza <ramiro@okteto.com>
2023-08-25 08:24:31 -0700

    linting code

--- oauth/app.py
+++ oauth/app.py
@@ -1,31 +1,29 @@
+import json
+import os
+import base64
+
 from flask import Flask, request, g, redirect, url_for
 from oauthlib.oauth2 import WebApplicationClient
 from oauthlib.common import add_params_to_uri
 
 import requests
-import json
-import os
-import socket
-import random
-import json
-import collections
-import base64
 
 GOOGLE_CLIENT_ID = os.environ.get('OAUTH2_CLIENT_ID', None)
 GOOGLE_CLIENT_SECRET = os.environ.get('OAUTH2_CLIENT_SECRET', None)
-GOOGLE_AUTHORIZATION_ENDPOINT='https://accounts.google.com/o/oauth2/v2/auth'
+GOOGLE_AUTHORIZATION_ENDPOINT = 'https://accounts.google.com/o/oauth2/v2/auth'
 GOOGLE_TOKEN_ENDPOINT = 'https://oauth2.googleapis.com/token'
-GOOGLE_USERINFO_ENDPOINT= 'https://openidconnect.googleapis.com/v1/userinfo'
+GOOGLE_USERINFO_ENDPOINT = 'https://openidconnect.googleapis.com/v1/userinfo'
 
 PUBLIC_URL = os.environ.get('PUBLIC_URL', None)
 
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
 app = Flask(__name__)
 
+
 @app.route('/oauth2')
 def login():
     print('received login request')
-    redirect_url=PUBLIC_URL
+    redirect_url = PUBLIC_URL
 
     state = request.args.get('state')
     if not state:
@@ -38,14 +36,16 @@ def login():
         state=state
     )
 
-    print('redirecting to ' + request_uri + ' with redirect_url ' + redirect_url)
+    print('redirecting to ' + request_uri +
+          ' with redirect_url ' + redirect_url)
     return redirect(request_uri)
+
 
 @app.route('/oauth2/callback')
 def callback():
     request_url = request.url.replace('http://', 'https://')
     request_base_url = request.base_url.replace('http://', 'https://')
-    
+
     code = request.args.get('code')
     state = request.args.get('state')
     if not state:
@@ -80,22 +80,25 @@ def callback():
         return 'User email not available or not verified by Google.', 400
 
     print('successfully authenticated ' + users_email)
-    redirect_url=decoded_state_json['prd']
+    redirect_url = decoded_state_json['prd']
 
-    user = dict(id_=unique_id, name=users_name, email=users_email, profile_pic=picture)
+    user = dict(id_=unique_id, name=users_name,
+                email=users_email, profile_pic=picture)
     state = base64.b64encode(json.dumps(user).encode('utf-8'))
-    
-    # This is not secure. A production proxy should pass this information via a database or another secure mechanism. This is only for demo purposes.
+
+    # This is not secure. A production proxy should pass this information via a database
+    # or another secure mechanism. This is only for demo purposes.
     params = [('state', state)]
     redirect_url_with_state = add_params_to_uri(redirect_url, params)
     return redirect(redirect_url_with_state)
+
 
 if __name__ == '__main__':
     extra_files = []
     if 'development' == os.getenv('FLASK_ENV'):
         app.jinja_env.auto_reload = True
         app.config['TEMPLATES_AUTO_RELOAD'] = True
-        extra_files=[
+        extra_files = [
             './static/stylesheets/style.css'
         ]
 
--- vote/app.py
+++ vote/app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template, request, make_response, jsonify, g, redirect, url_for
+import os
+import base64
+import socket
+import json
+
+from flask import Flask, render_template, request, make_response, redirect, url_for
 from flask_login import (
     LoginManager,
     current_user,
@@ -9,16 +14,7 @@ from flask_login import (
 )
 
 from oauthlib.common import add_params_to_uri
-import requests
 from redis import Redis, RedisError
-
-
-import os
-import base64
-import socket
-import random
-import json
-import collections
 
 
 PUBLIC_URL = os.environ.get('PUBLIC_URL', None)
@@ -26,7 +22,7 @@ OAUTH2_PROXY_URL = os.environ.get('OAUTH2_PROXY_URL', None)
 
 hostname = socket.gethostname()
 redis = Redis(host='redis', db=0)
-users = dict()
+users = {}
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(24)
@@ -41,41 +37,45 @@ class User(UserMixin):
         self.email = email
         self.profile_pic = profile_pic
 
-def getOptions():
+
+def get_options():
     option_a = 'Tacos'
     option_b = 'Burritos'
     return option_a, option_b
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return users.get(user_id)
 
-@app.route('/', methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if current_user.is_authenticated:
-        option_a, option_b = getOptions()
+        option_a, option_b = get_options()
 
         try:
-            votesA = int(redis.get(option_a) or 0) 
-            votesB = int(redis.get(option_b) or 0)
+            votes_a = int(redis.get(option_a) or 0)
+            votes_b = int(redis.get(option_b) or 0)
         except RedisError:
-            votesA = '<i>cannot connect to Redis, counter disabled</i>'
-            votesB = '<i>cannot connect to Redis, counter disabled</i>'
+            votes_a = '<i>cannot connect to Redis, counter disabled</i>'
+            votes_b = '<i>cannot connect to Redis, counter disabled</i>'
 
         if request.method == 'POST':
             try:
                 vote = request.form['vote']
                 if vote == 'a':
-                    votesA = redis.incr(option_a)
+                    votes_a = redis.incr(option_a)
                 else:
-                    votesB = redis.incr(option_b)
-            except Exception as e:
-                print(e)
-                votesA = '<i>An error occured</i>'
-                votesB = '<i>An error occured</i>'
+                    votes_b = redis.incr(option_b)
+            except Exception as ex:
+                print(ex)
+                votes_a = '<i>An error occured</i>'
+                votes_b = '<i>An error occured</i>'
 
-        with open('/var/run/secrets/kubernetes.io/serviceaccount/namespace', 'r') as fp:
-            namespace = fp.read()
+        with open('/var/run/secrets/kubernetes.io/serviceaccount/namespace',
+                  'r', encoding='utf-8') as file:
+            namespace = file.read()
 
         resp = make_response(render_template(
             'index.html',
@@ -84,12 +84,13 @@ def index():
             hostname=hostname,
             namespace=namespace,
             username=current_user.name,
-            votes_a=votesA,
-            votes_b=votesB,
+            votes_a=votes_a,
+            votes_b=votes_b,
         ))
         return resp
     else:
         return make_response(render_template('login.html'))
+
 
 @app.route('/oauth2')
 def login():
@@ -98,6 +99,7 @@ def login():
     redirect_url = add_params_to_uri(OAUTH2_PROXY_URL, params)
     print('redirecting request to proxy at ' + redirect_url)
     return redirect(redirect_url)
+
 
 @app.route('/oauth2/callback')
 def callback():
@@ -109,12 +111,14 @@ def callback():
     decoded_state_json = json.loads(base64.b64decode(state))
     print(decoded_state_json)
 
-    user = User(id_=decoded_state_json['id_'], name=decoded_state_json['name'], email=decoded_state_json['email'], profile_pic=decoded_state_json['profile_pic'])
+    user = User(id_=decoded_state_json['id_'], name=decoded_state_json['name'],
+                email=decoded_state_json['email'], profile_pic=decoded_state_json['profile_pic'])
     if decoded_state_json['id_'] not in users:
         users[decoded_state_json['id_']] = user
 
     login_user(user)
     return redirect(url_for('index'))
+
 
 @app.route('/logout')
 @login_required
@@ -122,12 +126,13 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 if __name__ == '__main__':
     extra_files = []
     if 'development' == os.getenv('FLASK_ENV'):
         app.jinja_env.auto_reload = True
         app.config['TEMPLATES_AUTO_RELOAD'] = True
-        extra_files=[
+        extra_files = [
             './static/stylesheets/style.css'
         ]
 
